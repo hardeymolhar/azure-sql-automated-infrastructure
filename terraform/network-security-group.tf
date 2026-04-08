00baae6c39830a29a8e049d2ce9f99d617bcac87
Emmanuel Ademola <48110058+hardeymolhar@users.noreply.github.com>
2026-04-08 16:13:04 +0100

    Refactor NSG and rule resources in Terraform

--- terraform/network-security-group.tf
+++ terraform/network-security-group.tf
@@ -54,3 +54,30 @@ resource "azurerm_network_security_rule" "rules" {
   resource_group_name         = local.primary_rg
   network_security_group_name = each.value.nsg.name
 }
+
+
+
+  
+  
+#  resource "azurerm_network_security_rule" "rules" {
+#
+#  for_each = local.nsg_rule_matrix
+#
+#  name     = "${each.value.rule.name}-${each.value.nsg_key}"
+#  priority = each.value.rule.priority
+#
+#  direction = each.value.rule.port == 1433 ? "Outbound" : "Inbound"
+#
+#  access   = "Allow"
+#  protocol = "Tcp"
+#
+#  source_port_range      = "*"
+#  destination_port_range = tostring(each.value.rule.port)
+#
+#  source_address_prefixes = each.value.rule.port == 1433 ? ["*"] : [local.client_ip]
+#
+#  destination_address_prefix = each.value.rule.port == 1433 ? "VirtualNetwork" : "*"
+#
+#  resource_group_name         = local.primary_rg
+#  network_security_group_name = each.value.nsg.name
+#}
